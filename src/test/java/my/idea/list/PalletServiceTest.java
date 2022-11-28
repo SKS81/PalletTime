cab3916bf276ce5aa809183619527aacc14c11b9
Aleksandr Vedeneev <a-vedeneev@ya.ru>
2022-11-29 00:29:35 +0300

    Modified PalletServiceTest.java file

--- src/test/java/my/idea/list/PalletServiceTest.java
+++ src/test/java/my/idea/list/PalletServiceTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
-public class TimeServiceTest {
+public class PalletServiceTest {
 
     PalletTimeService service = new PalletTimeService();
 
